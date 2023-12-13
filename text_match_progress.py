d30dbf5529d89d950e261ea50b44419dc2e82b19
cadmic <cadmic24@gmail.com>
2023-12-12 22:24:30 -0800

    Rewrite text_match_progress.py

--- text_match_progress.py
+++ text_match_progress.py
@@ -2,35 +2,65 @@
 
 import argparse
 import collections
+from collections.abc import Iterator
 from dataclasses import dataclass
 import difflib
 from enum import Enum
 import itertools
 from pathlib import Path
+import re
 import subprocess
 import sys
-
-class RelocType(Enum):
-    R_MIPS_LO16 = 1
-    R_MIPS_HI16 = 2
-    R_MIPS_26 = 3
 
 @dataclass
 class Inst:
     func_name: str
     mnemonic: str
-    args: list[str]
+    regs: list[str]
+    imm: int|None
     reloc_type: str|None
     reloc_symbol: str|None
 
-def run_objdump(filename):
-    # mips-linux-gnu-objdump -m mips:4300 -drz -j .text
+FUNC_RE = re.compile(r"([0-9a-f]+) <(.*)>:")
+
+def parse_func_name(line: str) -> str:
+    match = FUNC_RE.match(line)
+    if not match:
+        raise Exception(f"could not parse function name from '{line}'")
+    return match.group(2)
+
+def is_branch(mnemonic: str) -> bool:
+    return mnemonic.startswith("b") and mnemonic != "break"
+
+def parse_inst(func_name: str, line: str) -> Inst:
+    parts = line.split()
+    mnemonic = parts[2]
+    regs = []
+    imm = None
+    if len(parts) > 3:
+        for part in parts[3].split(","):
+            if "(" in part:  # load/store
+                offset, rest = part.split("(")
+                regs.append(rest[:-1])
+                imm = int(offset, 10)
+            else:
+                try:
+                    # branches use hex offsets from start of file
+                    imm = int(part, 16 if is_branch(mnemonic) else 0)
+                except ValueError:
+                    regs.append(part)
+    return Inst(func_name, mnemonic, regs, imm, None, None)
+
+def run_objdump(path: Path) -> list[Inst]:
+    if not path.exists():
+        raise Exception(f"file {path} does not exist")
+
     command = [
         "mips-linux-gnu-objdump",
         "-drz",
         "-m", "mips:4300",
         "-j", ".text",
-        str(filename),
+        str(path),
     ]
     try:
         lines = subprocess.run(
@@ -43,8 +73,9 @@ def run_objdump(filename):
     except subprocess.CalledProcessError as e:
         return []
 
-    func_name = None
     result = []
+
+    func_name = None
     i = 6  # skip preamble
     while i < len(lines):
         row = lines[i]
@@ -54,104 +85,79 @@ def run_objdump(filename):
             continue
 
         if not row.startswith(" "):
-            label = row.split()[-1]
-            # strip '<' and '>:'
-            func_name = label[1:-2]
+            func_name = parse_func_name(row)
             continue
 
         if not func_name:
             raise Exception(f"no function name for line '{row}'")
 
-        parts = row.split()
-        mnemonic = parts[2]
-        args = []
-        if len(parts) > 3:
-            for part in parts[3].split(","):
-                if "(" in part:
-                    # move immediate to the end
-                    offset, rest = part.split("(")
-                    args.append(rest[:-1])
-                    args.append(offset)
-                else:
-                    args.append(part)
+        inst = parse_inst(func_name, row)
 
-        reloc_type = None
-        reloc_symbol = None
-        while i < len(lines) and lines[i].startswith("\t"):
+        if i < len(lines) and lines[i].startswith("\t"):
             reloc = lines[i]
             i += 1
+            _, inst.reloc_type, inst.reloc_symbol = reloc.split()
 
-            reloc_symbol = reloc.split()[-1]
-            if "R_MIPS_LO16" in reloc:
-                reloc_type = RelocType.R_MIPS_LO16
-            elif "R_MIPS_HI16" in reloc:
-                reloc_type = RelocType.R_MIPS_HI16
-            elif "R_MIPS_26" in reloc:
-                reloc_type = RelocType.R_MIPS_26
-            else:
-                raise Exception(f"unknown relocation '{reloc}' for line '{row}'")
+        result.append(inst)
 
-        result.append(Inst(func_name, mnemonic, args, reloc_type, reloc_symbol))
-
+    # trim trailing nops
+    while result and result[-1].mnemonic == "nop":
+        result.pop()
     return result
 
-def trim_trailing_nops(lines):
-    while lines and lines[-1].mnemonic == "nop":
-        lines.pop()
-
-def diff_lines(lines1, lines2):
+def pair_instructions(insts1: list[Inst], insts2: list[Inst]) -> Iterator[tuple[Inst|None, Inst|None]]:
     differ = difflib.SequenceMatcher(
-        a=[line.mnemonic for line in lines1],
-        b=[line.mnemonic for line in lines2],
+        a=[(inst.func_name, inst.mnemonic) for inst in insts1],
+        b=[(inst.func_name, inst.mnemonic) for inst in insts2],
         autojunk=False)
     for (tag, i1, i2, j1, j2) in differ.get_opcodes():
-        for line1, line2 in itertools.zip_longest(lines1[i1:i2], lines2[j1:j2]):
-            yield (line1, line2)
+        for inst1, inst2 in itertools.zip_longest(insts1[i1:i2], insts2[j1:j2]):
+            yield (inst1, inst2)
 
-def should_ignore_diff(line1, line2):
+def has_diff(inst1: Inst, inst2: Inst) -> bool:
     if (
-        line1.mnemonic == line2.mnemonic
-        and line1.args[:2] == line2.args[:2]
-        and line1.reloc_type == line2.reloc_type
+        inst1.func_name != inst2.func_name
+        or inst1.mnemonic != inst2.mnemonic
+        or inst1.regs != inst2.regs
     ):
+        return True
+
+    if inst1.reloc_type == inst2.reloc_type:
         # ignore symbol differences
-        return True
+        return False
 
-    if line1.mnemonic == line2.mnemonic and line1.mnemonic.startswith("b"):
-        return True
+    if is_branch(inst1.mnemonic):
+        # ignore branch target differences
+        return False
 
-    return False
+    return inst1 != inst2
 
-def report_file(version, file):
+def find_functions_with_diffs(version: str, object_file: str):
     expected_dir = Path("expected/build") / version
     build_dir = Path("build") / version
 
-    lines1 = run_objdump(expected_dir / file)
-    lines2 = run_objdump(build_dir / file)
-
-    trim_trailing_nops(lines1)
-    trim_trailing_nops(lines2)
+    insts1 = run_objdump(expected_dir / object_file)
+    insts2 = run_objdump(build_dir / object_file)
 
     functions_with_diffs = collections.OrderedDict()
-    for line1, line2 in diff_lines(lines1, lines2):
-        if line1 is None:
-            functions_with_diffs[line2.func_name] = True
-        elif line2 is None:
-            functions_with_diffs[line1.func_name] = True
-        elif line1 != line2:
-            if not should_ignore_diff(line1, line2):
-                functions_with_diffs[line1.func_name] = True
-                functions_with_diffs[line2.func_name] = True
+    for inst1, inst2 in pair_instructions(insts1, insts2):
+        if inst1 is None and inst2 is not None:
+            functions_with_diffs[inst2.func_name] = True
+        elif inst1 is not None and inst2 is None:
+            functions_with_diffs[inst1.func_name] = True
+        elif inst1 is not None and inst2 is not None and has_diff(inst1, inst2):
+            functions_with_diffs[inst1.func_name] = True
+            functions_with_diffs[inst2.func_name] = True
 
     if not functions_with_diffs:
-        print(f"{file}: no diffs")
+        print(f"no diffs")
         return
 
-    print(f"{file} functions with diffs:")
+    print(f"{object_file} functions with diffs:")
     for func_name in functions_with_diffs:
         print(f"  {func_name}")
 
-def report_all_files(version):
+def print_summary_csv(version: str):
     expected_dir = Path("expected/build") / version
     build_dir = Path("build") / version
 
@@ -159,36 +165,29 @@ def report_all_files(version):
     for object_file in sorted(expected_dir.glob("src/**/*.o")):
         path = object_file.relative_to(expected_dir)
 
-        lines1 = run_objdump(expected_dir / path)
-        lines2 = run_objdump(build_dir / path)
-
-        trim_trailing_nops(lines1)
-        trim_trailing_nops(lines2)
-
-        if not lines1 or not lines2:
-            continue
+        insts1 = run_objdump(expected_dir / path)
+        insts2 = run_objdump(build_dir / path)
 
         added = 0
         removed = 0
         changed = 0
-        for line1, line2 in diff_lines(lines1, lines2):
-            if line1 is None:
+        for inst1, inst2 in pair_instructions(insts1, insts2):
+            if inst1 is None and inst2 is not None:
                 added += 1
-            elif line2 is None:
+            elif inst1 is not None and inst2 is None:
                 removed += 1
-            elif line1 != line2:
-                if not should_ignore_diff(line1, line2):
-                    changed += 1
+            elif inst1 is not None and inst2 is not None and has_diff(inst1, inst2):
+                changed += 1
 
-        print(f"{path},{len(lines1)},{len(lines2)},{added},{removed},{changed}")
+        print(f"{path},{len(insts1)},{len(insts2)},{added},{removed},{changed}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate progress matching .text sections")
     parser.add_argument("-v", "--version", help="version to compare", required=True)
-    parser.add_argument("-f", "--file", help="object file to compare")
+    parser.add_argument("-f", "--file", help="find functions with diffs given an object file to compare")
     args = parser.parse_args()
 
     if args.file is not None:
-        report_file(args.version, args.file)
+        find_functions_with_diffs(args.version, args.file)
     else:
-        report_all_files(args.version)
+        print_summary_csv(args.version)
